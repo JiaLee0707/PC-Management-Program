1cf5b77f4eba677f58949c84a6e1fc4df8507d21
JiaLee0707 <s2018s37@e-mirim.hs.kr>
2020-05-02 22:36:44 +0900

    DB 인코딩 수정

--- src/DB.java
+++ src/DB.java
@@ -1,6 +1,7 @@
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class DB {
@@ -13,6 +14,22 @@ public class DB {
 			Class.forName("org.gjt.mm.mysql.Driver").newInstance();
 			conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/pcmanagement", "root", "apmsetup");
 			System.out.println("DB 연결 완료");
+			
+			String sql = "insert into member (name, id) values (?, ?)";
+			pstmt = conn.prepareStatement(sql);
+			pstmt.setString(1, "이지아이이잉");
+			pstmt.setString(2, "이이이잉");
+		
+			pstmt.executeUpdate();
+			
+			sql = "select * from member";
+			pstmt = conn.prepareStatement(sql);
+			ResultSet srs = pstmt.executeQuery();
+			while(srs.next()) {
+				System.out.print(srs.getString("name")+" ");
+				System.out.print(srs.getString("id")+" ");
+				System.out.println();
+			}
 		} catch (SQLException ex) {
 			System.out.println("SQLException:" + ex);
 		} catch (Exception ex) {
